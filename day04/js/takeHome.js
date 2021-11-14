b4ada18e8dddb808fed0df595e1716ca247ad4d1
Hayat Jemal <htjemal2000@gmail.com>
2021-11-14 22:13:29 +0300

    update

--- day04/js/takeHome.js
+++ day04/js/takeHome.js
@@ -1,3 +1,8 @@
 function largestNumber() {
-    let fNumber=prompt("please choise a number b")
+    let fNumber=prompt("please choise a number b/n 1-100")
+    let lNumber = prompt("please add another number b/n 1-100");
+    fNumber = parseInt("fNumber");
+    lNumber = parseInt("lNumber");
+    
+
 }
