c5456a26eefb2ba244d461a8bb55fbd0a49d9da3
Hayat Jemal <htjemal2000@gmail.com>
2021-11-14 23:17:53 +0300

    update

--- day04/js/takeHome.js
+++ day04/js/takeHome.js
@@ -1,8 +1,11 @@
-function largestNumber() {
-    let fNumber=prompt("please choise a number b/n 1-100")
-    let lNumber = prompt("please add another number b/n 1-100");
-    fNumber = parseInt("fNumber");
-    lNumber = parseInt("lNumber");
-    
+function largerNumber() {
+    let fNumber = parseInt(prompt("please choise a number b/n 1-100"));
+    let lNumber = parseInt(prompt("please add another number b/n 1-100"));
+    if (fNumber > lNumber) {
+        alert(`${fNumber} is the large number`);
+    } else {
+        alert(`${lNumber} is the larger number`);
+    }
 
 }
+largerNumber()
