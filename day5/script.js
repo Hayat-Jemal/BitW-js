05f7e3f8276d7ec9a517e7f4dd02061dbc0f8cd9
Hayat Jemal <htjemal2000@gmail.com>
2021-11-16 18:30:18 +0300

    update

---
+++ day5/script.js
@@ -0,0 +1,12 @@
+function evenOrOdd(x) {
+    if (x % 2 === 0) {
+        alert(`${x} "is even number"`)
+    }else  alert(`${x} "is even number"`);
+}
+evenOrOdd();
+
+const isEven = (y) => {
+    if (y % 2 == 0) return true;
+    else return false;
+}
+alert(isEven(2));
