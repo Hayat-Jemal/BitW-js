cbffaa3b591a7943834e4a5ed307730f3adb326c
Hayat Jemal <htjemal2000@gmail.com>
2021-11-16 06:37:31 +0300

    üpdate

--- day04/js/takeHome.js
+++ day04/js/takeHome.js
@@ -10,6 +10,25 @@ function largerNumber() {
 }
 largerNumber()
 
+function askForNumber(whichNumber) {
+  const number = parseInt(prompt("Give me a number for number" + whichNumber));
+  if (isNaN(number)) {
+    return askForNumber(whichNumber);
+  }
+  return number;
+}
+
+const number1 = askForNumber(1);
+const number2 = askForNumber(2);
+
+if (number1 > number2) {
+  alert("number 1 is greater: " + number1 + " > " + number2);
+} else if (number2 > number1) {
+  alert("number 2 is greater: " + number1 + " < " + number2);
+} else {
+  alert("they are equal: " + number1 + " === " + number2);
+}
+
 
 function test() {
     alert("hello every one")
