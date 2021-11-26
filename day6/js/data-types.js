aeaa234ceff72c5eda5a681673c971d02ac93e8a
Hayat Jemal <htjemal2000@gmail.com>
2021-11-26 17:00:46 +0300

    add file

---
+++ day6/js/data-types.js
@@ -0,0 +1,17 @@
+// answers 
+const primiariveTypes = {
+    string: 'string',
+    number: 'number',
+    boolean: 'boolean',
+    Infinity: 'Infinity',
+}
+// sometimes we don't have the answer
+
+const falsyValues = {
+    boolean: false,
+    number: 0,
+    string: '',
+    undefined: undefined,
+    null: null,
+    NaN: NaN,
+}
