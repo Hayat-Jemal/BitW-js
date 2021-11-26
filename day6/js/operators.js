7680f2d4b8181168a456303b2cc3f31c072c3972
Hayat Jemal <htjemal2000@gmail.com>
2021-11-27 00:23:39 +0300

    add file

---
+++ day6/js/operators.js
@@ -0,0 +1,34 @@
+// operators
+
+// math operators
+// + - * / %
+1 + 1;
+1 - 1;
+1 * 1;
+1 / 1;
+1 % 1;
+
+// math operators with assignment
+// += -= *= /= %=
+let num = 1;
+num += 1; // 2
+num -= 1; // 1
+num *= 1; // 1
+num /= 1; // 1
+num %= 1; // 0
+
+// math operators with strings
+// + - * / %
+"1" + "1"; // "11" concatenation
+"1" - "1"; // 0
+"1" * "1"; // 1
+"4" / "2"; // 2
+"4" % "2"; // 0
+
+// math operators with booleans
+// + - * / %
+true + true; // 2
+true - true; // 0
+true * true; // 1
+true / true; // 1
+true % true; // 0
