21fd06f7eeaca90e44ea9f54c6d2e9aea9b59b63
Hayat Jemal <htjemal2000@gmail.com>
2021-11-30 23:41:09 +0300

    add file

--- day6/js/script.js
+++ day6/js/script.js
@@ -29,15 +29,15 @@
 //   });
 
 // ========================= EXAMPLE 2 =========================
-// function job(state) {
-//   return new Promise(function (resolve, reject) {
-//     if (state) {
-//       resolve("success");
-//     } else {
-//       reject("error");
-//     }
-//   });
-// }
+function job(state) {
+  return new Promise(function (resolve, reject) {
+    if (state) {
+      resolve("success");
+    } else {
+      reject("error");
+    }
+  });
+}
 
 let promise = job(true);
 
@@ -64,3 +64,18 @@ promise
   .catch(function (error) {
     console.log(error);
   });
+
+function fizzbuzz() {
+    for (let i = 0; i < 100; i++) {
+      if (i % 3 === 0 && i%5===0) {
+       console.log(i + " = " + "fizzBuzz");
+      } else
+        if (i % 3 === 0) {
+          console.log(i + " = " + "fizz");
+        } else
+          if(i % 5 === 0){
+          console.log(i + " = " + "buzz");
+          }else console.log(i)
+    }
+}
+    fizzbuzz()
