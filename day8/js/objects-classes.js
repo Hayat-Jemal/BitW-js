438226e93dc2f8b202308ebef3e4c97a14ca9b94
Hayat Jemal <htjemal2000@gmail.com>
2021-12-02 23:30:52 +0300

    update file

--- day8/js/objects-classes.js
+++ day8/js/objects-classes.js
@@ -1,23 +1,81 @@
-const object = {
-    key: 'value',
-    key2: 2,
-    key3: true,
-    key4: {},
-    key5: [1, 2, 3],
-};
+// const object = {
+//     key: 'value',
+//     key2: 2,
+//     key3: true,
+//     key4: {},
+//     key5: [1, 2, 3],
+// };
 
-const person = {
-    name: 'John',
-    age: 30,
-    isMarried: false,
-    ['key' + '2']: 'value2',
+// const person = {
+//     name: 'John',
+//     age: 30,
+//     isMarried: false,
+//     ['key' + '2']: 'value2',
+// }
+
+// console.log(person, object);
+
+// const liObject = {
+//     element: null,
+//     score: 0,
+//     createElement() {
+//         this.element = document.createElement('li');
+//         document.body.appendChild(this.element);
+//         this.element.innerHTML = this.score;
+//         this.element.addEventListener('click', () => {
+//             console.log(this);
+//             this.updateScore();
+//         });
+//     },
+//     updateScore() {
+//         this.score += 1;
+//         this.element.innerHTML = this.score;
+//         if (this.score > 10) {
+//             this.element.style.color = 'red';
+//             this.element.remove();
+//         }
+//     }
+// }
+
+// const liObject2 = {
+//     element: null,
+//     score: 0,
+//     createElement() {
+//         this.element = document.createElement('li');
+//         document.body.appendChild(this.element);
+//         this.element.innerHTML = this.score;
+//         this.element.addEventListener('click', () => {
+//             console.log(this);
+//             this.updateScore();
+//         });
+//     },
+//     updateScore() {
+//         this.score += 1;
+//         this.element.innerHTML = this.score;
+//         if (this.score > 10) {
+//             this.element.style.color = 'red';
+//             this.element.remove();
+//         }
+//     }
+// }
+
+// liObject.createElement();
+// liObject2.createElement();
+
+function createMyLi() {
+    const myLi = document.createElement('li');
+    myLi.innerHTML = 'Hello!';
+    document.body.appendChild(myLi);
 }
 
-console.log(person, object);
+createMyLi();
+createMyLi();
+createMyLi();
 
-const liObject = {
-    element: null,
-    score: 0,
+
+class MyLi {
+    element = null;
+    score = 0;
     createElement() {
         this.element = document.createElement('li');
         document.body.appendChild(this.element);
@@ -25,39 +83,26 @@ const liObject = {
         this.element.addEventListener('click', () => {
             console.log(this);
             this.updateScore();
+            console.dir(this.element);
         });
-    },
+    }
     updateScore() {
-        this.score += 1;
+        this.score += 100;
         this.element.innerHTML = this.score;
-        if (this.score > 10) {
+        if (this.score > 1000) {
             this.element.style.color = 'red';
             this.element.remove();
         }
     }
 }
 
-const liObject2 = {
-    element: null,
-    score: 0,
-    createElement() {
-        this.element = document.createElement('li');
-        document.body.appendChild(this.element);
-        this.element.innerHTML = this.score;
-        this.element.addEventListener('click', () => {
-            console.log(this);
-            this.updateScore();
-        });
-    },
-    updateScore() {
-        this.score += 1;
-        this.element.innerHTML = this.score;
-        if (this.score > 10) {
-            this.element.style.color = 'red';
-            this.element.remove();
-        }
-    }
-}
+const li1 = new MyLi();
+const li2 = new MyLi();
+const li3 = new MyLi();
 
-liObject.createElement();
-liObject2.createElement();
+li1.createElement();
+li2.createElement();
+li3.createElement();
+console.log('object', liObject, liObject2)
+console.log(li1, li2, li3);
+  
