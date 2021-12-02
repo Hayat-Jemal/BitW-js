0220c778d029432bf64763ab5dbeb36742fc5862
Hayat Jemal <htjemal2000@gmail.com>
2021-12-02 06:36:50 +0300

    update file

--- day07/js/arrays-loops-dom-listners.js
+++ day07/js/arrays-loops-dom-listners.js
@@ -35,3 +35,53 @@ function checkIfCorrect() {
     }
   }
 }
+function lightUpBox(color) {
+  const box = document.querySelector(`.${color}`); // querySelector to get the box with the color
+  box.classList.add("active"); // add the active class to the box
+  setTimeout(() => {
+    box.classList.remove("active"); // remove the active class from the box
+  }, 500); // after 500ms
+}
+
+function playCompAnswers(index) {
+  if (compAnswers[index]) {
+    lightUpBox(compAnswers[index]);
+    setTimeout(() => {
+      playCompAnswers(index + 1);
+    }, 1000);
+  }
+}
+
+function createColorBox(color) {
+  const box = document.createElement("div");
+  box.classList.add("color-box");
+  div.appendChild(box);
+  box.classList.add(color);
+  box.addEventListener("click", () => {
+    // callback function
+    userAnswers.push(color);
+    lightUpBox(color);
+    setTimeout(() => {
+      checkIfCorrect();
+    }, 1000);
+  });
+}
+
+function pickRandomColor() {
+  const randomIndex = Math.floor(Math.random() * colors.length); // random index
+  const color = colors[randomIndex]; // get the color at the random index
+  compAnswers.push(color); // push the color to the compAnswers array
+  userAnswers = [];
+  playCompAnswers(0);
+}
+
+function createStartButton() {
+  const startButton = document.createElement("button");
+  startButton.innerText = "Start";
+  document.body.appendChild(startButton);
+
+  startButton.addEventListener("click", () => {
+    pickRandomColor();
+    playCompAnswers(0);
+  });
+}
