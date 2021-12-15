4d3d453378a08d37941e0632c34af38328cf6dc0
Hayat Jemal <htjemal2000@gmail.com>
2021-12-16 00:49:20 +0300

    add folder

---
+++ day10/js/main.js
@@ -0,0 +1,38 @@
+import { getRandomLocation, getRandomUser, getRandomName } from "./api.js";
+import { phonePattern } from "./validations.js";
+import {
+  buildCardElements,
+  createAlphaButtons,
+  createLocationButtons,
+  createPile,
+  placeCardsOnPile,
+} from "./dom-helpers.js";
+
+import { createCardDeck, shuffleDeck } from "./cards.js";
+
+const deck = createCardDeck();
+shuffleDeck(deck);
+buildCardElements(deck);
+const piles = [
+  {
+    pile: createPile(),
+    deck: deck.slice(0, 26),
+    top: "200px",
+    name: "Player 1",
+  },
+  {
+    pile: createPile(),
+    deck: deck.slice(26),
+    top: "400px",
+    name: "Player 2",
+  },
+];
+
+placeCardsOnPile(piles[0]);
+placeCardsOnPile(piles[1]);
+
+// getRandomName().then(name => {
+//     console.log(name);
+//     createLocationButtons(name);
+// }) ;
+// createAlphaButtons();
