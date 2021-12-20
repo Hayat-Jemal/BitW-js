0dba149ebcefcf140c2396f362e589f35580e308
Hayat Jemal <htjemal2000@gmail.com>
2021-12-20 19:54:00 +0300

    update

--- day10/js/dom-helpers.js
+++ day10/js/dom-helpers.js
@@ -24,3 +24,62 @@ export function createLocationButtons(location) {
     }
   }
 }
+
+document.addEventListener("keydown", (event) => {
+  const { key, code } = event;
+  console.log(key, code);
+
+  const button = document.querySelector(`button[data-letter="${key}"]`);
+  if (button) {
+    button.disabled = true;
+
+    const buttons = document.querySelectorAll(
+      `button[data-answer-letter="${key}"]`
+    );
+    console.log(buttons);
+
+    buttons.forEach((item) => {
+      item.style.color = "black";
+    });
+  }
+});
+
+export function buildCardElements(deck) {
+  deck.forEach((card) => {
+    const element = document.createElement("div");
+    element.classList.add("card");
+    document.body.appendChild(element);
+    element.classList.add("flipped");
+    if (["hearts", "diams"].includes(card.suit)) {
+      element.classList.add("red");
+    }
+    element.innerHTML = `
+        <div class="value">${card.value}</div>
+        <div class="suit">&${card.suit};</div>
+        <div class="value">${card.value}</div>
+        `;
+    card.element = element;
+    element.addEventListener("click", () => {
+      element.classList.toggle("flipped");
+      const flippedCards = [
+        ...document.querySelectorAll(".card:not(.flipped)"),
+      ];
+      console.log(flippedCards);
+      if (flippedCards.length === 2) {
+        flippedCards.sort((a, b) => {
+          if (a.value === b.value) {
+            return alert("Tie");
+          }
+          if (getValue(a.value) > getValue(b.value)) {
+            console.log(a.name);
+            return alert(a.name + " You win");
+          } else {
+            console.log(b.name);
+            return alert(b.name + " You win");
+          }
+        });
+      }
+    });
+  });
+}
+
