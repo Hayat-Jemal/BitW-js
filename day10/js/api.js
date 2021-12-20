01ff0d9e5cfd4c83e6015ada4dab5e491051069f
Hayat Jemal <htjemal2000@gmail.com>
2021-12-20 19:55:21 +0300

    add api file

---
+++ day10/js/api.js
@@ -0,0 +1,17 @@
+export function getRandomUser() {
+  return fetch("https://randomuser.me/api/")
+    .then((res) => res.json())
+    .then((data) => data.results[0]);
+}
+
+export function getRandomLocation() {
+  return getRandomUser()
+    .then((user) => user.location)
+    .then((location) => `${location.country}`);
+}
+
+export function getRandomName() {
+  return getRandomUser().then(
+    (user) => `${user.name.first}, ${user.name.last}`
+  );
+}
